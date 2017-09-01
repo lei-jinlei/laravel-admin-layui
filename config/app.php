6476e43ef5c9d58bf7264b2533b36ff1ddfd1ab3
雷金磊 <leijinlei_phper@163.com>
2017-09-01 17:31:58 +0800

    添加debugbar插件
    添加debugbar插件

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // laravel-debugbar 扩展
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
