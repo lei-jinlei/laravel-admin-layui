1ab974c1f8a6ba919f733c741aaa0db34cbf7bd7
lei-jinlei <leijinlei_phper@163.com>
2017-09-13 16:23:08 +0800

    Web 开发实战入门 ( Laravel 5.5 )
    Web 开发实战入门 ( Laravel 5.5 )学习，研究，使用homestead集成laravel运行环境

--- resources/views/_layouts/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-  <p>© 2015 By <a href="http://www.shiyanlou.com" target="_blank">www.shiyanlou.com</a></p>
-</footer>
--- resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('_layouts.default')
-
-@section('main')
-<div class="am-g am-g-fixed blog-g-fixed">
-  <div class="am-u-sm-12">
-      <h1>Welcome to ShiYanLou！</h1>
-  </div>
-</div>
-@stop
