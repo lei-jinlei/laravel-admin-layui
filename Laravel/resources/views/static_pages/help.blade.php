40876b7f46f3873a50af7d7f94cf2d745cf7108f
lei-jinlei <leijinlei_phper@163.com>
2017-09-15 17:24:54 +0800

    Revert "页面基本搭建，前端流程执行成功"
    This reverts commit 051829a3b2f573b706d02ee1e84d3e59236bbf7f.

--- Laravel/resources/views/static_pages/help.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.default')
-@section('title', '帮助')
-@section('content')
-  <h1>帮助页</h1>
-@stop
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -11,9 +11,6 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', 'staticPagesController@home');
-Route::get('/help', 'staticPagesController@help');
-Route::get('/about', 'staticPagesController@about');
+Route::get('/', function () {
+    return view('welcome');
+});
