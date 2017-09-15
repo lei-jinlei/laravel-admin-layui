fb180ad0ca486f6215808f14a015c18b525bb1b8
lei-jinlei <leijinlei_phper@163.com>
2017-09-15 17:26:06 +0800

    Revert "Revert "迁移users模型文件""
    This reverts commit 1516f850a0bc60bd4593f024e89422bd6d0067ef.

--- Laravel/app/Models/User.php
+++ Laravel/app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -26,4 +26,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $table = 'users';
 }
