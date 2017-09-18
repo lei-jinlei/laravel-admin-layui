ae330cb75ab9c69378399003f209d0d7e58bf5b0
lei-jinlei <leijinlei_phper@163.com>
2017-09-18 10:12:01 +0800

    测试
    测试

---
+++ Laravel/resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
+</html>
