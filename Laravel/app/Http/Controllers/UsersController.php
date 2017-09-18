17500aa5a38ed05ac95905daf7b6284dc164bf3c
lei-jinlei <leijinlei_phper@163.com>
2017-09-18 14:41:38 +0800

    添加路由

--- Laravel/app/Http/Controllers/UsersController.php
+++ Laravel/app/Http/Controllers/UsersController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
     }
 }
---
+++ Laravel/resources/views/users/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '注册')
+
+@section('content')
+{{ $user->name }} - {{ $user->email }}
+@stop
