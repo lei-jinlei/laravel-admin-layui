88ac94a312470b4ecd6b4740f47ea36be5d3dfaa
雷金磊 <leijinlei_phper@163.com>
2017-11-21 21:33:29 +0800

    去除原有项目，重新练习

--- resources/views/shared/_status_form.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{ route('statuses.store') }}" method="post">
-    @include('shared._errors')
-    {{ csrf_field() }}
-    <textarea rows="3" class="form-control" placeholder="聊聊新鲜事儿..." name="content">{{ old('content') }}</textarea>
-    <button type="submit" class="btn btn-primary pull-right">发布</button>
-</form>
--- resources/views/shared/_user_info.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ route('users.show', $user->id) }}">
-  <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
-</a>
-<h1>{{ $user->name }}</h1>
