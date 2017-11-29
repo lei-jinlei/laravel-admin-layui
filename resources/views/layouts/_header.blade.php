f4b2a4c53fef23d75c2aa36ff4cfc7d6485325f1
雷金磊 <leijinlei_phper@163.com>
2017-11-29 23:59:13 +0800

    搭建界面

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a href="{{ url('/') }}" class="navbar-brand">
+                LaraBBS
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+        </div>
+
+    </div>
+</nav>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_field() }}">
+
+    <title>@yield('title', 'laraBBS') - Laravel 进级教程</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ assert('css/app.css') }}">
+
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+
+        @include('layouts._header')
+
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+
+        @include('layouts._footer')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ assert('js/app.js') }}"></script>
+</body>
+
+</html>
