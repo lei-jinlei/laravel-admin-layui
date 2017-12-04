f1772ba919838f14b3292811f89c8a1d19bd1b84
雷金磊 <leijinlei_phper@163.com>
2017-12-04 23:05:52 +0800

    话题详情页

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -53,7 +53,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功');
 	}
 
 	public function destroy(Topic $topic)
@@ -61,7 +61,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '成功删除！');
 	}
 
 	public function uploadImage(Request $request, ImageUploadHandler $uploader)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content=" {{ csrf_token() }} ">
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
 
     <title> @yield('title', 'laraBBS') - Laravel 进级教程</title>
 
