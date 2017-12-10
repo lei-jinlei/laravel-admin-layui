a793513c02dc274573e27b33a32c17ca24d23785
雷金磊 <leijinlei_phper@163.com>
2017-12-10 15:19:05 +0800

    使用队列进行翻译api调用

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,8 +2,8 @@
 
 namespace App\Observers;
 
-use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -16,11 +16,16 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
 
         // 生成话题摘录
-        $topic->excerpt = make_excerpt($topic->body);
+        $topic->excerpt = make_excerpt($topic->body);  
+    }
 
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if (!$topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+    
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 
