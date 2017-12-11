3f2f26c481c2909e991d34a26d20f625878210ee
雷金磊 <leijinlei_phper@163.com>
2017-12-11 22:46:07 +0800

    删除回复，删除话题，关联处理

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -29,6 +29,11 @@ class TopicObserver
         }
     }
 
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
+
     public function creating(Topic $topic)
     {
         //
