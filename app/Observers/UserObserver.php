fd21602b7234fa737a9c219f69809f606a3feab6
雷金磊 <leijinlei_phper@163.com>
2017-12-23 16:19:07 +0800

    用户默认头像，首页

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,6 +9,14 @@ use App\Models\User;
 
 class UserObserver
 {
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'http://larabbs.app/uploads/images/avatars/default.png';
+        }
+    }
+
     public function creating(User $user)
     {
         //
